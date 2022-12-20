66d003b51784332ea92b5d9164154eb564f46ac5
FontaMatte <matteo.fontanel@gmail.com>
2022-12-20 21:28:37 +0100

    Added reset button to clear all input

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const userName = document.getElementById('userName');
 const kmRange = document.getElementById('kmRange');
 const userAge = document.getElementById('userAge');
 const submitButton = document.getElementById('submit-button');
+const resetButton = document.getElementById('reset-button');
 const kmPrice = 0.21;
 const minorsDiscount = 0.2;
 const elderyDiscount = 0.4;
@@ -17,23 +18,27 @@ const passengerName = document.getElementById('passengerName');
 submitButton.addEventListener('click', 
     function() {
         // Prezzo minori 18
-        if (userAge.value == 'meno18') {
+        if (userAge.value == "meno18") {
 
             ticketPrice = ((kmPrice * kmRange.value) - (kmPrice * kmRange.value) * minorsDiscount);
-            console.log(ticketPrice);
+
         }
 
         // Prezzo maggiori 65
-        else if (userAge.value == 'piu65') {
+        else if (userAge.value == "piu65") {
 
             ticketPrice = ((kmPrice * kmRange.value) - (kmPrice * kmRange.value) * elderyDiscount);
         }    
 
         // Prezzo >= 18 <= 65
-        else if (userAge.value == '18-65') {
+        else if (userAge.value == "18-65") {
 
             ticketPrice = kmPrice * kmRange.value;
-        }   
+        }
+        
+        else if (userAge.value == "scegli") 
+            alert("inserisci prima una fascia di eta'");
+        
 
         // Stampo in HTML il risultato
         ticketPrice = ticketPrice.toFixed(2); //arrotondo a 2 cifre decimali
@@ -42,6 +47,14 @@ submitButton.addEventListener('click',
         passengerName.innerHTML = `${userName.value}`;
     }
 )    
+
+resetButton.addEventListener('click',
+        function() {
+            document.getElementById('userName').value = "";
+            document.getElementById('kmRange').value = "";
+            document.getElementById('userAge').value = "scegli";
+        }
+)
 
 
 
